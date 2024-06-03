d0732e11bdc410a6dd58b9503c9946b21cba2d43
BaoulyNelson <elconquistadorbaoulyn@gmail.com>
2024-06-02 20:35:04 -0400

    2eme changement

---
+++ script/recherche.js
@@ -0,0 +1,104 @@
+function toggleSearch() {
+    var searchContainer = document.querySelector('.search-container');
+    var searchInput = document.getElementById('searchInput');
+    if (searchContainer.classList.contains('open')) {
+        searchContainer.classList.remove('open');
+    } else {
+        searchContainer.classList.add('open');
+        searchInput.focus(); // Mettre le focus sur le champ de recherche
+    }
+}
+
+
+// pour filtrer les liens et soumettre le formulaire
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchForm = document.querySelector('.form-inline');
+    const searchInput = document.getElementById('searchInput');
+    const navLinks = document.querySelectorAll('.nav-link');
+    const sectionContents = document.querySelectorAll('.section-content');
+    const messageContainer = document.getElementById('messageContainer');
+
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Empêche le rechargement de la page
+
+        const searchTerm = searchInput.value.toLowerCase().trim();
+        
+        // Vérifier s'il y a un terme de recherche
+        if (!searchTerm) {
+            messageContainer.textContent = 'Veuillez entrer un terme de recherche.';
+            return;
+        }
+        
+        let hasMatch = false;
+
+        // Masquer tous les contenus et réinitialiser les liens
+        sectionContents.forEach(function (section) {
+            section.style.display = 'none';
+        });
+
+        navLinks.forEach(function (link) {
+            const linkText = link.textContent.toLowerCase().trim();
+            const linkParent = link.parentElement;
+
+            if (linkText.includes(searchTerm)) {
+                linkParent.style.display = 'block';
+
+                // Afficher le contenu associé
+                const targetId = link.getAttribute('data-target');
+                const correspondingSection = document.getElementById(targetId);
+                correspondingSection.style.display = 'block';
+
+                hasMatch = true;
+            } else {
+                linkParent.style.display = 'none';
+            }
+        });
+
+        if (!hasMatch) {
+            messageContainer.textContent = 'Aucun résultat trouvé.';
+        } else {
+            messageContainer.textContent = '';
+        }
+    });
+
+    searchInput.addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase().trim();
+
+        if (searchTerm === '') {
+            messageContainer.textContent = ''; // Effacer le message lorsque le champ est vide
+            resetDisplay();
+            return;
+        }
+
+        let hasMatch = false;
+
+        navLinks.forEach(function (link) {
+            const linkText = link.textContent.toLowerCase().trim();
+            const linkParent = link.parentElement;
+
+            if (linkText.includes(searchTerm)) {
+                linkParent.style.display = 'block';
+                hasMatch = true;
+            } else {
+                linkParent.style.display = 'none';
+            }
+        });
+
+        if (!hasMatch) {
+            messageContainer.textContent = 'Aucun résultat trouvé.';
+        } else {
+            messageContainer.textContent = '';
+        }
+    });
+
+    function resetDisplay() {
+        navLinks.forEach(function (link) {
+            link.parentElement.style.display = 'block';
+        });
+
+        sectionContents.forEach(function (section) {
+            section.style.display = 'none';
+        });
+    }
+});
--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Menu toggle functionality
     const menuLink = document.querySelector('a[data-target="menu"]');
     const menuPanel = document.getElementById('menuPanel');
     const body = document.body;
@@ -36,10 +37,8 @@ document.addEventListener('DOMContentLoaded', function () {
             menuPanel.style.display = 'none';
         }
     });
-});
 
-
-document.addEventListener('DOMContentLoaded', function () {
+    // Navigation links functionality
     const navLinks = document.querySelectorAll('.nav-link');
     const sections = document.querySelectorAll('.section-content');
 
@@ -50,108 +49,16 @@ document.addEventListener('DOMContentLoaded', function () {
             // Retirer la classe active de toutes les sections
             sections.forEach(section => {
                 section.classList.remove('active');
+                section.style.display = 'none';
             });
 
             // Ajouter la classe active à la section correspondante
             const targetId = this.getAttribute('data-target');
-            document.getElementById(targetId).classList.add('active');
-        });
-    });
-});
-
-
-
-function toggleSearch() {
-    var searchContainer = document.querySelector('.search-container');
-    var searchInput = document.getElementById('searchInput');
-    if (searchContainer.style.display === 'flex') {
-        searchContainer.style.display = 'none';
-    } else {
-        searchContainer.style.display = 'flex';
-        searchInput.focus(); // Mettre le focus sur le champ de recherche
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    const searchForm = document.querySelector('.form-inline');
-    const searchInput = document.getElementById('searchInput');
-    const navLinks = document.querySelectorAll('.nav-link');
-    const sectionContents = document.querySelectorAll('.section-content');
-    const messageContainer = document.getElementById('messageContainer');
-
-    searchForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // Empêche le rechargement de la page
-
-        const searchTerm = searchInput.value.toLowerCase().trim();
-        let hasMatch = false;
-
-        // Masquer tous les contenus et réinitialiser les liens
-        sectionContents.forEach(function (section) {
-            section.style.display = 'none';
-        });
-
-        navLinks.forEach(function (link) {
-            const linkText = link.textContent.toLowerCase().trim();
-            const linkParent = link.parentElement;
-
-            if (linkText.includes(searchTerm)) {
-                linkParent.style.display = 'block';
-
-                // Afficher le contenu associé
-                const targetId = link.getAttribute('data-target');
-                const correspondingSection = document.getElementById(targetId);
-                correspondingSection.style.display = 'block';
-
-                hasMatch = true;
-            } else {
-                linkParent.style.display = 'none';
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                targetSection.classList.add('active');
+                targetSection.style.display = 'block';
             }
         });
-
-        if (!hasMatch || searchTerm === '') {
-            messageContainer.textContent = 'Aucun résultat trouvé.';
-        } else {
-            messageContainer.textContent = '';
-        }
     });
-
-    searchInput.addEventListener('input', function () {
-        const searchTerm = this.value.toLowerCase().trim();
-
-        if (searchTerm === '') {
-            messageContainer.textContent = '';
-            resetDisplay();
-            return;
-        }
-
-        let hasMatch = false;
-
-        navLinks.forEach(function (link) {
-            const linkText = link.textContent.toLowerCase().trim();
-            const linkParent = link.parentElement;
-
-            if (linkText.includes(searchTerm)) {
-                linkParent.style.display = 'block';
-                hasMatch = true;
-            } else {
-                linkParent.style.display = 'none';
-            }
-        });
-
-        if (!hasMatch) {
-            messageContainer.textContent = 'Aucun résultat trouvé.';
-        } else {
-            messageContainer.textContent = '';
-        }
-    });
-
-    function resetDisplay() {
-        navLinks.forEach(function (link) {
-            link.parentElement.style.display = 'block';
-        });
-
-        sectionContents.forEach(function (section) {
-            section.style.display = 'none';
-        });
-    }
 });
